6acf553da2b1d70c46a46c47d6502a769ffe3ec8
unknown <atellezro@gmail.com>
2016-06-11 14:22:27 +0200

    Estructura inicial

---
+++ src/main/java/com/hackaton/psd2/ContahackApplication.java
@@ -0,0 +1,12 @@
+package com.hackaton.psd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContahackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContahackApplication.class, args);
+	}
+}
