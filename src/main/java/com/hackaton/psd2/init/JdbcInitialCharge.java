882f19c470893c0c20bdf48f5f1e2fc07d1a1705
César Rodríguez Medina <cesinrm@gmail.com>
2016-06-11 16:46:54 +0200

    Initial charge

---
+++ src/main/java/com/hackaton/psd2/init/JdbcInitialCharge.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.hackaton.psd2.init;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+/**
+ * @author cesarrodriguezmedina
+ *
+ */
+@Configuration
+public class JdbcInitialCharge implements CommandLineRunner {
+
+	private static final Logger LOG = LoggerFactory.getLogger(JdbcInitialCharge.class);
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	public void run(String... strings) throws Exception {
+
+		LOG.info("Creating tables");
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		String schemaLocation = "/CONTAHACK.sql";
+		populator.addScript(new ClassPathResource(schemaLocation));
+		populator.setContinueOnError(false);
+		DatabasePopulatorUtils.execute(populator, this.dataSource);
+	}
+
+}
