402ea417c8d80bd6278c92af017400f763436384
atellezr <atellezro@gmail.com>
2016-06-12 11:00:10 +0200

    Front actualizado

--- src/main/resources/static/js/contahack.js
+++ src/main/resources/static/js/contahack.js
@@ -37,26 +37,77 @@
   }
 
   function loadMovements(me) {
+    var bankId = me.data('bankId');
+    var accId = me.data('accId');
     $.ajax({
       //url: "https://apisandbox.openbankproject.com/obp/v2.0.0/my/accounts",
-      url: "https://apisandbox.openbankproject.com/obp/v2.0.0/banks/"+me.data('bankId')+"/accounts/"+me.data('accId')+"/owner/transactions",
+      url: "https://apisandbox.openbankproject.com/obp/v2.0.0/banks/"+bankId+"/accounts/"+accId+"/owner/transactions",
       contentType: "application/json",
       beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'DirectLogin token="'+token+'"');},
     })
     .done(function(data, textStatus){
       //console.log(data);
-      printMovements(data);
+      printMovements(data, bankId, accId);
     })
     .fail(function(error){
       console.log(error);
     });
   }
 
-  function printMovements(movements) {
+  function printMovements(movements, bankId, accId) {
     console.log(movements);
+    movements = movements.transactions || movements;
     var container = $(".movements");
-    var cntMvts = $("<ul></ul>");
-    container.append();
+    container.find('table').remove();
+    container.find('.bankId').text(bankId);
+    container.find('.accId').text(accId);
+
+    var cntMvts = $('<table class="table table-striped"></table').
+      append('<thead><td>Etiqueta</td><td>Grupo contable</td><td>Contrapartida</td><td>Cantidad</td><td>Comentarios</td></thead>');
+    var mvt, tags, otherAcc, amount, comments, accountingGroup;
+    for (var i=0; i<movements.length; i++) {
+      mvt = movements[i];
+      tags = '';
+      var oa;
+      for (var j=0; j<mvt.metadata.tags.length; j++) {
+        tags += mvt.metadata.tags[j].value;
+      }
+
+      comments = '';
+      for (var j=0; j<mvt.metadata.comments.length; j++) {
+        comments += mvt.metadata.comments[j].value;
+      }
+
+      oa = $('<div class="row"></div>');
+      otherAcc = mvt.other_account.bank.name + '<br/>' + ( mvt.other_account.IBAN || '' ) + mvt.other_account.id;
+      if (mvt.other_account.metadata && mvt.other_account.metadata.image_URL) {
+        oa.append('<div class="col-md-2"><img style="max-height: 4em;" src="'+mvt.other_account.metadata.image_URL+'" /></div>');
+        oa.append('<div class="col-md-10">'+otherAcc+'</div>');
+      }
+      else {
+        oa.append('<div class="col-md-12">'+otherAcc+'</div>');
+      }
+      amount = mvt.details.value.amount + ' ' + mvt.details.value.currency;
+
+      if (tags.toLowerCase().indexOf('proveedoresmateriales') != -1
+          || tags.toLowerCase().indexOf('personalnominas') != -1
+          || tags.toLowerCase().indexOf('proveedoresalquileres') != -1
+          || tags.toLowerCase().indexOf('pagoproveedor') != -1
+        ) {
+          accountingGroup = '6';
+      }
+      else if (tags.toLowerCase().indexOf('proveedores') != -1 ) {
+        accountingGroup = '4';
+      }
+      else if (tags.toLowerCase().indexOf('cliente') != -1 ) {
+        accountingGroup = '7';
+      }
+
+
+      cntMvts.append('<tr><td>'+tags+'</td><td>'+accountingGroup+'</td><td>'+oa.html()+'</td><td>'+amount+'</td><td>'+comments+'</td></tr>');
+    }
+    container.append(cntMvts);
+    container.show();
   }
 
 
